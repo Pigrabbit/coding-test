4f840d40d83f07279fbe8b8fc03285172f7fe040
Pigrabbit <donghyuk1003@gmail.com>
2020-05-07 17:16:02 +0900

    update solution

--- dp/toSchool.cpp
+++ dp/toSchool.cpp
@@ -12,31 +12,37 @@ using namespace std;
 const int MAX_HEIGHT = 100, MAX_WIDTH = 100;
 int cache[MAX_HEIGHT + 1][MAX_WIDTH + 1];
 
+int height, width;
+
 int getPath(int x, int y, vector<vector<int>> & puddles) {
-    if (x == 0 || y == 0) {
+    if (x > width || y > height ) {
         return 0;
     }
-    if (x == 1 && y == 1) {
+    int & ret = cache[y][x];
+    if (ret != -1) {
+        cout << "cached! [" << y << "][" << x << "]\n";
+        return ret;
+    }
+    if (x == width && y == height) {
         return 1;
     }
     
-    int & ret = cache[y][x];
-    if (ret != -1) {
-        return ret;
-    }
-    ret = getPath(x - 1, y, puddles) + getPath(x, y - 1, puddles);
-    return ret % 1000000007;
+    
+
+    return  ret = (getPath(x + 1, y, puddles) + getPath(x, y + 1, puddles)) % 1000000007;
 }
 
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
+    width = m, height = n;
     memset(cache, -1, sizeof(cache));
+
     for (int i = 0; i < puddles.size(); i++) {
         int y = puddles[i][1], x = puddles[i][0]; 
         cache[y][x] = 0;
     }
-    answer = getPath(m, n, puddles);
 
+    answer = getPath(1, 1, puddles);
     return answer;
 }
 
